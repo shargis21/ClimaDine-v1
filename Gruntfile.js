9a502698d45bbe4b7386bc2d87950fecfea9eaf3
Shannon Hargis <shargis21@gmail.com>
2017-04-25 15:07:10 -0700

    added my github information...whoops

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function (grunt) {
       },
       pages: {
         options: {
-          remote: 'git@github.com:your_github_user/your_webapp.git',
+          remote: 'git@github.com:shargis21/ClimaDine-v1.git',
           branch: 'gh-pages'
         }
       }
