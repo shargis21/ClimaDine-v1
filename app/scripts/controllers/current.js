772d5c606e118fb7662534de5c80585caae320c7
Shannon Hargis <shargis21@gmail.com>
2017-05-20 14:49:02 -0700

    tried to add ngstorage and was not successful

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -8,7 +8,7 @@
  * Controller of the climaDineV1App
  */
 angular.module('climaDineV1App')
-    .controller('CurrentCtrl', function ($scope, $routeParams, current) {
+    .controller('CurrentCtrl', function ($scope, $routeParams, current, $localStorage) {
       $scope.cityID = $routeParams.cityID;
 
       $scope.currentWeather = current.query({
