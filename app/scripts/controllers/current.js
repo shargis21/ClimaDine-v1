ad5cb22ff7e8bea9b33975751798ef6145fb06e1
Shannon Hargis <shargis21@gmail.com>
2017-05-23 20:53:04 -0700

    added animations and tried to add save city functionality

--- app/scripts/controllers/current.js
+++ app/scripts/controllers/current.js
@@ -1,17 +1,38 @@
-'use strict';
+angular.module('weatherApp')
+  .controller('CurrentCtrl', function ($scope, $routeParams, CurrentWeather, $localStorage) {
+    $scope.cityID = $routeParams.cityID;
 
-/**
- * @ngdoc function
- * @name climaDineV1App.controller:CurrentCtrl
- * @description
- * # CurrentCtrl
- * Controller of the climaDineV1App
- */
-angular.module('climaDineV1App')
-    .controller('CurrentCtrl', function ($scope, $routeParams, current, $localStorage) {
-      $scope.cityID = $routeParams.cityID;
-
-      $scope.currentWeather = current.query({
-          cityID: $routeParams.cityID
-      });
+    $scope.currentWeather = CurrentWeather.query({
+        cityID: $routeParams.cityID
     });
+
+    $scope.saveCity = function(city){
+        var cityData = {
+            'name': city.name,
+            'id': city.id
+        };
+        if (!$localStorage.savedCities){
+            $localStorage.savedCities = [cityData];
+        } else {
+            // Check to make sure we haven't already saved the city.
+            var save = true;
+            for (var i=0; i < $localStorage.savedCities.length; i++){
+                if ($localStorage.savedCities[i].id === cityData.id) {
+                    // this is a duplicate, so don't save
+                    save = false;
+                }
+            }
+            if (save===true){
+                $localStorage.savedCities.push(cityData);
+                $scope.citySaved = {
+                    'success': true
+                };
+            } else {
+                console.log('city already saved');
+                $scope.citySaved = {
+                    'duplicate': true
+                };
+            }
+        }
+    };
+  });
